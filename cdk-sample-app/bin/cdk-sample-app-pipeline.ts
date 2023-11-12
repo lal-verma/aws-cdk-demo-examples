0bcb798cd92513876019e267fce9b057ea90234d
lal verma <lal.verma@gmail.com>
2023-11-12 00:15:32 -0600

    initial commit: 1.2

--- cdk-sample-app/bin/cdk-sample-app-pipeline.ts
+++ cdk-sample-app/bin/cdk-sample-app-pipeline.ts
@@ -5,7 +5,7 @@ import { SamplePipelineStack } from '../lib/pipeline/sample-pipeline-stack';
 const app = new cdk.App();
 new SamplePipelineStack(app, 'SamplePipelineStack', {
   env: {
-    account: '480935361548',
+    account: '123456789',
     region: 'us-east-2',
   }
 });
